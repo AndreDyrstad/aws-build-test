ddec8b6d6d0e817207ff5c600f661539fffced43
André Dyrstad <ady@computas.com>
2020-08-20 12:48:54 +0200

    Init application

---
+++ src/test/kotlin/com/example/demo/AwsTestApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		val a = 1
+		val b = 2
+		assertEquals(a+b, 3)
+	}
+}
